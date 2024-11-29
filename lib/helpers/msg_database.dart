1090b9a9dabb04a3a1d7ef01b7ed6f5cba627add
Tobias Brandner <tobias-brandner@gmx.at>
2024-11-29 19:02:43 +0100

    Objectbox & Tflite dependencies
    Hat einfach Stunden gedauert

--- lib/helpers/msg_database.dart
+++ lib/helpers/msg_database.dart
@@ -1,4 +1,22 @@
 
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:objectbox/objectbox.dart';
+
+@Entity()
+class ChatMessage {
+  int id = 0; // ObjectBox requires an ID field; 0 is auto-incremented.
+
+  String sender;
+  String message;
+
+  ChatMessage({
+    required this.sender,
+    required this.message,
+  });
+}
+//  
+
 void addToDatabase(String name, String message) {
   print("Message '" + message + "' from " + name + " was succesfully added to database.");
 }
+
